7de2b1eb854457de73926698e90b45f29df5c978
sharmavaibhav0103 <61970310+sharmavaibhav0103@users.noreply.github.com>
2020-08-06 19:39:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.use(express.static(path.join(__dirname,'/public')));
 
 //Database Configuaration
 db();
+app.options("/*", function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.sendStatus(200);
+});
+console.log("Vaibhav Sharma");
 
 //Homepage Routes
 app.get('/',(req,res) => {
